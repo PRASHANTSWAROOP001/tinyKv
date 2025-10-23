b11ee4b8a4d345d97e976b7c75870e946dde3d9a
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-10-23 17:53:17 +0530

    added the graceful handling of end of line error

--- db/storage.go
+++ db/storage.go
@@ -2,43 +2,53 @@ package db
 
 import ("io" 
 "encoding/binary"
+"bytes"
 )
 
-func ReadRecord(r io.Reader)(*Record, int, error){
-	var deletedFlag byte
-	if err := binary.Read(r, binary.LittleEndian, &deletedFlag); err != nil{
-		return  nil,0, err
-	}
-	
-	var keyLen, valLen uint32
+func ReadRecord(r io.Reader) (*Record, int, error) {
+    header := make([]byte, 1+4)
+    n, err := io.ReadFull(r, header)
+    if err == io.EOF || err == io.ErrUnexpectedEOF {
+        return nil, n, io.EOF // just stop gracefully
+    } else if err != nil {
+        return nil, n, err
+    }
 
-	if err := binary.Read(r, binary.LittleEndian, &keyLen); err != nil{
-		return nil,0,err
-	}
+    buf := bytes.NewReader(header)
+    var deleted byte
+    var keyLen uint32
+    binary.Read(buf, binary.LittleEndian, &deleted)
+    binary.Read(buf, binary.LittleEndian, &keyLen)
 
-	if err := binary.Read(r, binary.LittleEndian, &valLen); err != nil{
-		return nil,0,err
-	}
+    key := make([]byte, keyLen)
+    n2, err := io.ReadFull(r, key)
+    if err == io.EOF || err == io.ErrUnexpectedEOF {
+        return nil, n + n2, io.EOF
+    } else if err != nil {
+        return nil, n + n2, err
+    }
 
-	keyBytes := make([]byte, keyLen)
+    valLenBytes := make([]byte, 4)
+    n3, err := io.ReadFull(r, valLenBytes)
+    if err == io.EOF || err == io.ErrUnexpectedEOF {
+        return nil, n + n2 + n3, io.EOF
+    } else if err != nil {
+        return nil, n + n2 + n3, err
+    }
 
-	if _,err := io.ReadFull(r,keyBytes); err != nil{
-		return nil,0,err
-	}
+    valLen := binary.LittleEndian.Uint32(valLenBytes)
 
+    val := make([]byte, valLen)
+    n4, err := io.ReadFull(r, val)
+    if err == io.EOF || err == io.ErrUnexpectedEOF {
+        return nil, n + n2 + n3 + n4, io.EOF
+    } else if err != nil {
+        return nil, n + n2 + n3 + n4, err
+    }
 
-	valBytes := make([]byte, valLen)
-
-	if _,err := io.ReadFull(r,valBytes); err != nil{
-		return nil,0,err
-	}
-
-	rec := &Record{
-		Deleted: deletedFlag ==1,
-		Key: string(keyBytes),
-		Value: string(valBytes),
-	}
-
-	totalBytes := 1 + 4 + 4 + int(keyLen) + int(valLen)
-    return rec, totalBytes, nil
+    return &Record{
+        Key:     string(key),
+        Value:   string(val),
+        Deleted: deleted == 1,
+    }, n + n2 + n3 + n4, nil
 }
