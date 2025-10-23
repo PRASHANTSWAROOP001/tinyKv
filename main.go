38b5455d981412162568f3b9828fe7954dd3ea9c
PRASHANTSWAROOP001 <mrprashantswaroop@gmail.com>
2025-10-23 17:54:06 +0530

    tested concurrent access working

--- main.go
+++ main.go
@@ -3,26 +3,54 @@ package main
 import (
 	"fmt"
 	"github.com/PRASHANTSWAROOP101/tinyKv/db"
+	"log"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
 	database, err := db.Open("./data")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer database.Close()
 
-	database.Set("name", "Prashant")
-	value, ok1 := database.Get("password")
-	if !ok1{
-		fmt.Println("missing data")
-	}else{
-		fmt.Println(value)
+	var wg sync.WaitGroup
+
+	keys := []string{"alpha", "beta", "gamma", "delta", "epsilon"}
+	values := []string{"one", "two", "three", "four", "five"}
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for j := 0; j < 50; j++ {
+				k := keys[rand.Intn(len(keys))] + fmt.Sprintf("-%d", j)
+				v := values[rand.Intn(len(values))] + fmt.Sprintf("-%d", id)
+				if err := database.Set(k, v); err != nil {
+					log.Printf("[writer-%d] Set error: %v\n", id, err)
+				}
+				time.Sleep(time.Millisecond * 20)
+			}
+		}(i)
 	}
-	val, ok := database.Get("name")
-	if ok {
-		fmt.Println("name:", val)
-	} else {
-		fmt.Println("not found")
+
+	// Reader goroutines
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			for j := 0; j < 100; j++ {
+				k := keys[rand.Intn(len(keys))] + fmt.Sprintf("-%d", rand.Intn(50))
+				if val, ok := database.Get(k); ok {
+					log.Printf("[reader-%d] got key=%s val=%s\n", id, k, val)
+				}
+				time.Sleep(time.Millisecond * 15)
+			}
+		}(i)
 	}
+
+	wg.Wait()
+	fmt.Println("✅ Concurrent test completed successfully")
 }
